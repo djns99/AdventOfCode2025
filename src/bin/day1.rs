9c326bbac996036f161f871c2281ba7335f2f0af
stokes <40156487+djns99@users.noreply.github.com>
2025-12-02 19:26:55 +1300

    Day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -17,11 +17,24 @@ fn part2(input: &Vec<String>) {
     let answer = input.iter().fold((0, 50), |curr, line| {
         let direction = if line.chars().next() == Some('L') { -1 } else { 1 };
         let shift = line[1..].parse::<i32>().unwrap() * direction;
-        let unwrapped = curr.1 + shift;
-        let wrapped = unwrapped.rem_euclid(100);
-        let diff = ((unwrapped - wrapped) / 100).abs();
-        let diff = if unwrapped <= 0 { diff + 1 } else { diff };
-        return (curr.0 + diff.abs(), wrapped);
+        // Entire revolutions we can ignore
+        let mut shift_revolutions = shift.abs() / 100;
+
+        // Remaining shift to apply
+        let extra_shift = shift % 100;
+
+        // The new position
+        let mut new_pos = curr.1 + extra_shift;
+
+        // If we have extra shift that will get us an extra zero, add it
+        if extra_shift != 0 && curr.1 != 0 && (new_pos <= 0 || new_pos >= 100) {
+            new_pos = new_pos.rem_euclid(100);
+            shift_revolutions += 1;
+        }
+        new_pos = new_pos.rem_euclid(100);
+        println!("State {:?} + shift {:?} ({:?} * 100 + {:?}) = {:?} with {:?} + {:?} total zeros", curr, shift, shift.abs() / 100, extra_shift, new_pos, curr.0, shift_revolutions);
+
+        return (curr.0 + shift_revolutions, new_pos);
     });
     println!("Answer is {:?}", answer);
 }
